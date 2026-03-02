5a8c51630ee0e8f345b80bd0e4b48d4cc3dd1698
Jakub Trmota <jakub@trmota.cz>
2026-03-02 22:21:57 +0100

    Functions: remove dumping real value in asserts

--- src/functions.php
+++ src/functions.php
@@ -2,56 +2,56 @@
 
 function as_int(mixed $value): int
 {
-	assert(is_int($value), 'Integer value expected, got ' . gettype($value) . ' ' . var_export($value, true));
+	assert(is_int($value), 'Integer value expected, got ' . gettype($value));
 	return $value;
 }
 
 
 function as_int_nullable(mixed $value): int|null
 {
-	assert($value === null || is_int($value), 'Integer value or null expected, got ' . gettype($value) . ' ' . var_export($value, true));
+	assert($value === null || is_int($value), 'Integer value or null expected, got ' . gettype($value));
 	return $value;
 }
 
 
 function as_float(mixed $value): float
 {
-	assert(is_float($value), 'Float value expected, got ' . gettype($value) . ' ' . var_export($value, true));
+	assert(is_float($value), 'Float value expected, got ' . gettype($value));
 	return $value;
 }
 
 
 function as_float_nullable(mixed $value): float|null
 {
-	assert($value === null || is_float($value), 'Float value or null expected, got ' . gettype($value) . ' ' . var_export($value, true));
+	assert($value === null || is_float($value), 'Float value or null expected, got ' . gettype($value));
 	return $value;
 }
 
 
 function as_bool(mixed $value): bool
 {
-	assert(is_bool($value), 'Bool value expected, got ' . gettype($value) . ' ' . var_export($value, true));
+	assert(is_bool($value), 'Bool value expected, got ' . gettype($value));
 	return $value;
 }
 
 
 function as_bool_nullable(mixed $value): bool|null
 {
-	assert($value === null || is_bool($value), 'Bool value or null expected, got ' . gettype($value) . ' ' . var_export($value, true));
+	assert($value === null || is_bool($value), 'Bool value or null expected, got ' . gettype($value));
 	return $value;
 }
 
 
 function as_string(mixed $value): string
 {
-	assert(is_string($value), 'String value expected, got ' . gettype($value) . ' ' . var_export($value, true));
+	assert(is_string($value), 'String value expected, got ' . gettype($value));
 	return $value;
 }
 
 
 function as_string_nullable(mixed $value): string|null
 {
-	assert($value === null || is_string($value), 'String value or null expected, got ' . gettype($value) . ' ' . var_export($value, true));
+	assert($value === null || is_string($value), 'String value or null expected, got ' . gettype($value));
 	return $value;
 }
 
@@ -60,7 +60,7 @@ function as_type(mixed $value, string $type): mixed
 {
 	assert(
 		Forrest79\TypeValidator\Helpers\Runtime::check($type, Forrest79\StrictTypes\Helpers\RuntimeSourceFilename::detectFilenameCallback(), $value),
-		$type . ' value expected, got ' . gettype($value) . ' ' . var_export($value, true),
+		$type . ' value expected, got ' . gettype($value),
 	);
 	return $value;
 }
